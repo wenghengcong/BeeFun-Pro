af56264c286a0012ca01dabfc17ac49d340eb9bd
wenghengcong <wenghengcong@gmail.com>
2015-12-30 07:31:14 +0800

    1.add file folder 2.add storyboard file

---
+++ Coderpursue/Coderpursue/VIewController/Profile/CPLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CPLoginViewController.swift
+//  Coderpursue
+//
+//  Created by wenghengcong on 15/12/30.
+//  Copyright © 2015年 JungleSong. All rights reserved.
+//
+
+import UIKit
+
+class CPLoginViewController: CPBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

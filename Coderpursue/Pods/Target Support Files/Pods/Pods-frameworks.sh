ad6ab23591dcf81a2ab1be941b68cf50826f5094
wenghengcong <wenghengcong@gmail.com>
2016-01-08 05:35:39 +0800

    add swiftjson lib

--- Coderpursue/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Coderpursue/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/IQKeyboardManagerSwift.framework"
+  install_framework "Pods/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/IQKeyboardManagerSwift.framework"
+  install_framework "Pods/SwiftyJSON.framework"
 fi

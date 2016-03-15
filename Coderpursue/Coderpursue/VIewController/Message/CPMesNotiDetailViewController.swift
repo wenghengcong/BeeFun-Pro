205b97944a7d429a2fd902e7791113865ecb78ee
wenghengcong <wenghengcong@gmail.com>
2016-03-15 22:17:53 +0800

    >add basic auth test code >add notification detail view ,but the work will move next release

---
+++ Coderpursue/Coderpursue/VIewController/Message/CPMesNotiDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CPMesNotiDetailViewController.swift
+//  Coderpursue
+//
+//  Created by WengHengcong on 3/15/16.
+//  Copyright © 2016 JungleSong. All rights reserved.
+//
+
+import UIKit
+
+class CPMesNotiDetailViewController: CPBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

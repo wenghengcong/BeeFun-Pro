039342529941d5237b0f4decdfac3c0fa9082c8c
wenghengcong <wenghengcong@gmail.com>
2017-03-12 16:22:09 +0800

    更好新的share管理

---
+++ Coderpursue/Coderpursue/Manager/InstanceManager.swift
@@ -0,0 +1,24 @@
+//
+//  InstanceManager.swift
+//  Coderpursue
+//
+//  Created by WengHengcong on 2017/3/12.
+//  Copyright © 2017年 JungleSong. All rights reserved.
+//
+
+import UIKit
+
+/// 应用的代理
+let cpAppDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+
+
+/// 当前顶部视图控制器
+let cpTopViewController = cpAppDelegate.tabBarController?.topViewController()
+
+
+/// 当前顶部导航栏控制器
+let cpTopNavigationViewController = cpAppDelegate.tabBarController?.currentNavigationViewController()
+
+
+/// 当前顶部视图
+let cpTopView = cpTopViewController?.view

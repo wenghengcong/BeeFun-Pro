0ab31ce699a98ecd92c216829989abad1f8a881a
wenghengcong <wenghengcong@icoud.com>
2019-09-01 17:58:14 +0800

    update

--- BeeFun/BeeFun/SystemManager/ProConfig/BFThirdLibKey.swift
+++ BeeFun/BeeFun/SystemManager/ProConfig/BFThirdLibKey.swift
@@ -7,19 +7,19 @@
 //
 
 // MARK: - Apple  https://developer.apple.com/library/archive/qa/qa1633/_index.html
-public let AppleAppID = "1393808664"
+public let AppleAppID = ""
 public let SocialAppStore = "https://itunes.apple.com/app/id1393808664"
 public let BeeFunAppStore = "itms-apps://itunes.apple.com/app/id1393808664"
 public let AppBundleIdentifier = "com.luci.beefun.ios"
-public let APNsAuthKeyID = "QW2AYPNFMK"
+public let APNsAuthKeyID = ""
 
 // MARK: - BeeFun
 public let AppOfficeSite = "http://www.beefun.top"
 public let AppOfficeShortSite = "www.beefun.top"
 
 // MARK: - Github
-public let GithubAppClientId = "294e97e63b0e68f456ad"
-public let GithubAppClientSecret = "d208e4cc6b9afcdea382e6afae58c8b27bf18377"
+public let GithubAppClientId = ""
+public let GithubAppClientSecret = ""
 public let GithubAppRedirectUrl = "beefunios://www.beefun.top"
 //https://developer.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/
 public let GithubAppScope = "user,repo,notifications,gist,admin:repo_hook,admin:org_hook,admin:org"
@@ -31,48 +31,48 @@ public let ShortSocailGithub = "http://t.cn/Rqvjd6W"
 public let ShortOfficeWebsite = "http://t.cn/R0QpGhp"
 
 // MARK: 微博
-public let WeiboSDKAppKey = "1224249220"
-public let WeiboSDKAppSecret = "45b9b45c5e29ecc1127858fb076d05ae"
+public let WeiboSDKAppKey = ""
+public let WeiboSDKAppSecret = ""
 
 // MARK: - 腾讯
 // MARK: Bugly
-public let TencentBuglyAppID = "900022223"
+public let TencentBuglyAppID = ""
 
 // MARK: 微信
-public let WeiXinSDKAppID = "wx9231f7e12ddac379"
-public let WeiXinSDKAppSecret = "972001c30c2a2c62d70dbfc8fc671d0b"
+public let WeiXinSDKAppID = ""
+public let WeiXinSDKAppSecret = ""
 
 // MARK: 腾讯开发平台
-public let TencentSDKAppID = "1106086248"
-public let TencentSDKAppKey = "lmSYALCN1XjiG7XN"
+public let TencentSDKAppID = ""
+public let TencentSDKAppKey = ""
 
 // MARK: - 阿里
 // MARK: 友盟
-public let UMengAppSecret = "56eb5109e0f55a2e0f002f84"
-public let SocailGithub = "https://github.com/wenghengcong/BeeFun"
+public let UMengAppSecret = ""
+public let SocailGithub = "https://github.com/wenghengcong/BeeFun-Pro"
 
 // MARK: 钉钉
 public let DingTalkSDKAppID = ""
 public let DingTalkSDKAppSecret = ""
 
 // MARK: - Share SDK
-public let ShareSDKAppKey = "11b9cf1d0cec0"
-public let ShareSDKAppSecret = "480ba09dc992ae0234a0fef9183ff836"
+public let ShareSDKAppKey = ""
+public let ShareSDKAppSecret = ""
 
 // MARK: - 极光
 // https://www.jiguang.cn/
-public let JPushAppKey = "d0f8652fe84f0fbfbde51cdc"
-public let JPushAppSecret = "464bd0df32ad4e66edb60506"
+public let JPushAppKey = ""
+public let JPushAppSecret = ""
 public let JPushChannel = "App Store"
 
 // MARK: - 国外服务
 // MARK:facebook
-public let FackbookSDKAppID = "789261007875015"
-public let FackbookSDKAppSecret = "b29f205f582c617fe26421b626c84576"
+public let FackbookSDKAppID = ""
+public let FackbookSDKAppSecret = ""
 
 // MARK:  twitter
-public let TwitterSDKConsumerKey = "CDdurbc23ffDDs6VlBMOkX8oH"
-public let TwitterSDKConsumerSecret = "cythqJqoFnpgQTkUcetALuG6agOKZ3bil2AYGAb2KJjDPU7Ndx"
+public let TwitterSDKConsumerKey = ""
+public let TwitterSDKConsumerSecret = ""
 
 // MARK:  linkedin
 public let LinkedInSDKAppID = ""
